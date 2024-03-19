128e93739e78c5b3202479e08cb31acbee24afcc
lucasbalbino09 <balbino2409@gmail.com>
2024-03-19 20:58:31 -0300

    subindo comentario.

--- public/app.js
+++ public/app.js
@@ -1,16 +1,16 @@
-fetch('http://localhost:8080/filmes')
-    .then(response => response.json())
+fetch('http://localhost:8080/filmes') // função que vai receber os dados. 
+    .then(response => response.json()) // Convertendo a resposta para Json.
     .then(data => {
-        const filmesList = document.getElementById('filmes-list');
+        const filmesList = document.getElementById('filmes-list'); // variavel que recebe a div.
         data.forEach(filme => {
-            const filmeDiv = document.createElement('div');
+            const filmeDiv = document.createElement('div'); // criando um novo elemento de interação. 
             filmeDiv.innerHTML = `
                 <h2>${filme.nome}</h2>
                 <img src="${filme.foto}" alt="${filme.nome}">
                 <p><strong>Descrição:</strong> ${filme.descricao}</p>
                 <p><strong>Elenco:</strong> ${filme.elenco}</p>
             `;
-            filmesList.appendChild(filmeDiv);
+            filmesList.appendChild(filmeDiv); // Recebendo as alteraçãoes da div. 
         });
     })
     .catch(error => console.error('Erro ao carregar os filmes:', error));
